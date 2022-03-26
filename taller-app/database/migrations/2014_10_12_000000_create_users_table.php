813cc54b53911012b679138cebe1335f0581de3d
José Alí Rivas Gómez <josealirivas2110@gmail.com>
2022-03-26 15:14:10 -0600

    Models and MFS done for users, clients, pieces and workorder states

--- taller-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ taller-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('phone');
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
